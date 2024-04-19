60709cefe4dd29e41a1f460804fb568ba82f0e7c
a.korneev <a.korneev@dprim.ru>
2024-04-19 14:45:26 +1000

    updated

--- handlers.py
+++ handlers.py
@@ -30,19 +30,7 @@ async def calculate_offer(message: Message):
     await message.answer(text='Введите сумму авто (в йенах)')
 
 
-# @router.message()
-# async def calculate_offer(message: Message):
-#     if message.text.isdigit():
-#         car_price = int(message.text)
-#         amount = calculate_offer_1(car_price)
-#         await message.answer(text=
-#                              f'Сумма расходов в Японии:\n <b>{amount}</b> рублей, из них:\n'
-#                              f'Цена машины: <b>{car_price}</b>\n'
-#                              f'Фрахт: <b>400$</b>\n',
-#                              parse_mode='html')
-
-
-@router.callback_query()
+@router.callback_query(lambda callback_query: callback_query.data == 'get_currency')
 async def current_currency(callback_query: types.CallbackQuery):
     try:
         response = requests.get(exchange_rate_url)
@@ -56,11 +44,11 @@ async def current_currency(callback_query: types.CallbackQuery):
         jpy_rate = data['rates']['JPY']
         cny_rate = data['rates']['CNY']
 
-        exchange_rate_message = f'Текущий курс валют относительно рубля на <b>{date_time}</b>:\n\n' \
-                                f'Евро: <b>{round((1 / eur_rate), 2)}</b> руб.\n' \
-                                f'Доллар США: <b>{round((1 / usd_rate), 2)}</b> руб.\n'\
-                                f'Японская Иена: <b>{round((1 / jpy_rate), 2)}</b> руб.\n' \
-                                f'Китайский Юань: <b>{round((1 / cny_rate), 2)}</b> руб.\n'
+        exchange_rate_message = f'Текущий курс валют на <b>{date_time}</b>:\n\n' \
+                                f'Евро: <b>{round((1 / eur_rate), 2)}</b> ₽\n' \
+                                f'Доллар США: <b>{round((1 / usd_rate), 2)}</b> ₽\n'\
+                                f'Японская Иена: <b>{round((1 / jpy_rate), 2)}</b> ₽\n' \
+                                f'Китайский Юань: <b>{round((1 / cny_rate), 2)}</b> ₽\n'
 
         await callback_query.answer()
         await callback_query.message.answer(exchange_rate_message, parse_mode='html')
