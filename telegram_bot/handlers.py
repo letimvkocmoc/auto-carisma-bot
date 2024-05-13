013ffbb527628130c3b13f30bc21f42645f9efee
a.korneev <a.korneev@dprim.ru>
2024-05-13 17:42:22 +1000

    updated

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -3,11 +3,12 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from database.utils import SQL
 from core.utils import calculate_offer
-from keyboards.inline import set_admin_inline_keyboard
+from keyboards.inline import set_admin_inline_keyboard, create_link
 
 router = Router()
 admin_ids = [588120104, 367150414]
 admin_keyboard = set_admin_inline_keyboard()
+sql = SQL()
 
 
 @router.message(Command('start'))
@@ -61,3 +62,28 @@ async def get_japan_consumptions(message: types.Message):
             await message.answer(f'Произошла ошибка: {e}.')
     else:
         await message.edit_text('Вы не админ.')
+
+
+@router.callback_query(lambda callback_query: callback_query.data == 'orders')
+async def get_orders(callback_query: types.CallbackQuery):
+    if callback_query.from_user.id in admin_ids:
+        try:
+            orders = sql.get_orders()
+            for order in orders:
+                keyboard = create_link(1, link=order[10], text='Ссылка на аукцион', id=order[0])
+                await callback_query.message.answer_photo(photo=f"{order[9]}",
+                                                          caption=f"🔎 Заявка <b>№ {order[0]}</b>\n"
+                                                                  f"🔹 Имя: {order[2]}\n"
+                                                                  f"🔹 Фамилия: {order[3]}\n"
+                                                                  f"🔹 Телеграм ID: {order[1]}\n"
+                                                                  f"☎️ Номер телефона: {order[4]}\n"
+                                                                  f"🚗 Марка авто: {order[5]}\n"
+                                                                  f"⭐️ Оценка: {order[6]}\n"
+                                                                  f"💵 Стоимость: {order[7]}\n"
+                                                                  f"📍 Статус: {order[8]}\n"
+                                                                  f"💰 Оплачен: {'✅' if order[11] else '❌'}\n",
+                                                          parse_mode='html', reply_markup=keyboard)
+        except Exception as e:
+            await callback_query.message.answer(text=f'Какая то хуйня: {e}')
+    else:
+        await callback_query.message.answer(text='Вы не админ.')
--- telegram_bot/keyboards/inline.py
+++ telegram_bot/keyboards/inline.py
@@ -1,4 +1,5 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def set_admin_inline_keyboard():
@@ -6,6 +7,16 @@ def set_admin_inline_keyboard():
         inline_keyboard=[
             [InlineKeyboardButton(text='Рассчитать стоимость расходов по Японии',
                                   callback_data='get_japan_consumptions')],
-            [InlineKeyboardButton(text='Текущий курс валют', callback_data='get_currency')]
+            [InlineKeyboardButton(text='Текущий курс валют', callback_data='get_currency')],
+            [InlineKeyboardButton(text='Список заявок', callback_data='orders')]
         ])
     return keyboard
+
+
+def create_link(width: int, link, text: str | None = None, id: str | None = None):
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    kb_builder.row(
+        InlineKeyboardButton(text=text, web_app=WebAppInfo(url=link)),
+        InlineKeyboardButton(text='Открыть на сайте', url='http://127.0.0.1:5000/edit/{id}'.format(id=id)),
+        width=width)
+    return kb_builder.as_markup()
